30021115d2386c00ea6e5d342a25cd4f8b93a6d5
Jacob Allen <45643741+jakes1403@users.noreply.github.com>
2024-10-21 18:47:32 -0500

    Auto commit

--- _next/static/VXXTfsFT_OsAhw8eWlhw-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/494-7c7ec7e439c936ff.js","static/chunks/pages/index-51da78d58848f4fe.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/WCaxediuinuj9ifUVj2yx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/793-1d1cccc65b5c6ec8.js","static/chunks/pages/index-b8c00154cc198431.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
